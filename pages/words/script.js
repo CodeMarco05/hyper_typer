bac97d4ce1f668cb6d217503040655a43fc347be
Marco Brandt <marcobrandt@MBP-von-Marco.fritz.box>
2023-12-27 21:31:23 +0100

    working_prototype

--- pages/words/script.js
+++ pages/words/script.js
@@ -1,18 +1,75 @@
+
+let inputField = document.getElementById('inputField')
+
+let words;
+
+inputField.addEventListener('input', async function () { //handles every change in the textfield 
+    let inputValue = inputField.value;
+    if (inputValue[inputValue.length - 1] == ' ') {
+        let validWord = checkValid(inputValue)
+        if (validWord) {
+            await updateScreen()
+        }
+    }
+})
+
+async function updateScreen() {
+    words.shift();
+    let jsonFilePath = "../../data.json";
+
+    try {
+        let response = await fetch(jsonFilePath);
+
+        // Check if the request was successful (status code 200)
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        // Parse the JSON response
+        let data = await response.json();
+
+        // Implement logic for loading random words
+        await words.push(data.words[getRandomNumber(0, data.words.length)]);
+    } catch (error) {
+        // Handle errors
+        console.error('Error loading JSON:', error);
+    }
+
+    //updates the inputField 
+    let inputField = document.getElementById('inputField')
+    inputField.value = ''
+
+    let display = document.getElementById('wordDisplay')
+    let string = ''
+    words = Array.from(words)
+    for (let i = 0; i < words.length; i++) {
+        string = string.concat(' ', words[i]); // Use += to concatenate strings
+    }
+    display.textContent = string
+}
+
+function checkValid(input) {
+    if (input.trim() == words[0]) {
+        return true
+    }
+    return false
+}
+
+//this funcition is called when the html document is loaded
 window.onload = async function () {
-    let words = await getRandomWords(50); 
+    words = await getRandomWords(8);  //gets random 20 words -> await because it is asynchronous code
 
     let display = document.getElementById('wordDisplay');
     let string = '';
 
     words = Array.from(words)
-    
+
     for (let i = 0; i < words.length; i++) {
         string = string.concat(' ', words[i]); // Use += to concatenate strings
     }
-    
-    display.textContent = string
 
-    console.log("finished");
+    display.textContent = string
+    console.log("Finished Loading")
 }
 
 async function getRandomWords(amount) {
@@ -33,7 +90,7 @@ async function getRandomWords(amount) {
 
         // Implement logic for loading random words
         for (let i = 0; i < amount; i++) {
-            words.push(data.words[getRandomNumber(0, 2)]); //adapt the number to the elements for the words
+            words.push(data.words[getRandomNumber(0, data.words.length)]);
         }
     } catch (error) {
         // Handle errors
