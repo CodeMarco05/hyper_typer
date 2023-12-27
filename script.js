02fd29181cd49c180663bfec36f0c41c58832696
CodeExpertin <mariedesiree@web.de>
2023-12-27 14:24:29 +0100

    test

---
+++ script.js
@@ -0,0 +1,10 @@
+function sentences(){
+    window.location.href = "./pages/sentences.html"
+}
+function words(){
+    window.location.href = "./pages/words.html"
+}
+function statistic(){
+    window.location.href = "./pages/statistic.html"
+    
+}
